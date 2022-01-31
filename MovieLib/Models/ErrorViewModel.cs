e278d6bae34f898532751a32a0dca93659276a20
michaelpiscione <piscionemichael@gmail.com>
2022-01-31 08:41:37 -0700

    init

---
+++ MovieLib/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MovieLib.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
